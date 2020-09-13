0eb74bc7cb34306d335ae142fd3a3933df2f4dbd
Ghamdan Mohsin <chemist2ever@gmail.com>
2020-09-13 23:18:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,7 +1,5 @@
 # prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
-
-def key_for_min_value(name_hash)
+# Given a hash with numeric values, return the key for the smallest va
 def key_for_min_value(hash)
   smallest_key = nil
   tiniest_value = nil
