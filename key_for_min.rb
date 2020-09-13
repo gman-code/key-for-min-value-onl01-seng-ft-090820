67f6a8f452384af624cbea6ff0918969848507ef
Ghamdan Mohsin <chemist2ever@gmail.com>
2020-09-13 23:17:31 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,5 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-name_hash.key(name_hash.values.min)
+
 end
-key_for_min_value_my_try(ikea)
